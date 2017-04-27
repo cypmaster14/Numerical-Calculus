8c05907935657b2962ea0fa3c946e2a874919411
cypmaster14 <lila.lazar@info.uaic.ro>
2017-04-27 03:37:38 +0300

    Add GUI for H5,H7

--- Tema5/Main.py
+++ Tema5/Main.py
@@ -1,5 +1,13 @@
-from IterativeInverse import IterativeInverse
+from Tema5.IterativeInverse import IterativeInverse
 import numpy as np
+from tkinter import *
+import tkinter.messagebox as messagebox
+
+root = Tk()
+lbl1 = Label(root)
+lbl2 = Label(root)
+text = Text()
+
 
 def compute_epsilon():
     m = -1
@@ -9,6 +17,35 @@ def compute_epsilon():
         m -= 1
     return u
 
+
+def button_click():
+    text.delete('1.0', END)
+    main()
+
+
+def initUi(parent):
+    parent.title("Tema 5")
+    global lbl1, lbl2, text
+
+    frame1 = Frame(parent)
+    frame1.pack(fill=X)
+
+    lbl1 = Label(frame1, text="Norma:", width=30)
+    lbl1.pack(side=LEFT, padx=5, pady=5)
+
+    lbl2 = Label(frame1, text="Iteratii:", width=30)
+    lbl2.pack(side=LEFT, padx=5, pady=5)
+
+    button = Button(frame1, text="Calculate", width=10, command=button_click)
+    button.pack(padx=5, pady=5)
+
+    frame2 = Frame(parent)
+    frame2.pack(fill=X)
+
+    text = Text(frame2, height=200, width=900)
+    text.pack(padx=5, pady=5)
+
+
 def main():
     a = []
     with open("matrix.txt") as f:
@@ -17,7 +54,18 @@ def main():
             line = [int(x) for x in line_s.split(" ")]
             a.append(line)
     a = np.array(a)
-    ii = IterativeInverse(n,compute_epsilon(),1000,a)
-    ii.solve(3)
+    ii = IterativeInverse(n, compute_epsilon(), 1000, a)
+    result = ii.solve(3)
+    if len(result) == 1:
+        messagebox.showinfo("Error", "Matrix is divergent")
+    else:
+        print("Result:", result)
+        lbl1['text'] = "Norm:" + str(result[0])
+        lbl2['text'] = "Iterations:" + str(result[1])
+        text.insert(INSERT, "Inverse:\n{}\nNumpyInverse:\n{}".format(result[2], result[3]))
 
-main()
+
+if __name__ == "__main__":
+    root.geometry("900x700+300+300")
+    initUi(root)
+    root.mainloop()
--- Tema7/Main.py
+++ Tema7/Main.py
@@ -2,18 +2,22 @@ from numpy.random import uniform
 from numpy import sort
 from numpy import append
 from numpy import pi
-import Functions
-import Aitken
-import Trigonometry
+import Tema7.Functions as Functions
+import Tema7.Aitken as Aitken
+import Tema7.Trigonometry as Trigonometry
 import matplotlib.pyplot as matplot
+from tkinter import *
+
+root = Tk()
+text = Text()
 
 
 def get_Xs():
     with open("input.txt") as f:
         x0, xn, n = [int(z) for z in next(f).split()]
-    x = uniform(x0,xn,n-1)
-    x = append(x,x0)
-    x = append(x,xn)
+    x = uniform(x0, xn, n - 1)
+    x = append(x, x0)
+    x = append(x, xn)
     x = sort(x)
     x = x.tolist()
     return x
@@ -35,26 +39,26 @@ def get_Fs2(x):
 
 def get_approx_f_aitken(n, x, f):
     approx_f = []
-    for i in range(0,n+1):
-        approx_f.append(Aitken.Ln(n,x,f,x[i]))
+    for i in range(0, n + 1):
+        approx_f.append(Aitken.Ln(n, x, f, x[i]))
     return approx_f
 
 
 def get_approx_f_trigonometry(n, x, f):
     approx_f = []
-    for i in range(0,n+1):
-        approx_f.append(Trigonometry.solve(n,x,f,x[i]))
+    for i in range(0, n + 1):
+        approx_f.append(Trigonometry.solve(n, x, f, x[i]))
     return approx_f
 
 
-def draw_plot_aitken(x,f,approx_f):
-    matplot.plot(x,f,'r--',x,approx_f,'b--')
+def draw_plot_aitken(x, f, approx_f):
+    matplot.plot(x, f, 'r--', x, approx_f, 'b--')
     matplot.ylabel('Aitken')
     matplot.show()
 
 
-def draw_plot_trigonometry(x,f,approx_f):
-    matplot.plot(x,f,'r--',x,approx_f,'b--')
+def draw_plot_trigonometry(x, f, approx_f):
+    matplot.plot(x, f, 'r--', x, approx_f, 'b--')
     matplot.ylabel('Trigonometry')
     matplot.show()
 
@@ -64,24 +68,50 @@ def main_aitken():
     f = get_Fs1(x)
     n = len(x) - 1
     x_new = 6
-    approx_fx = Aitken.Ln(n,x,f,x_new)
-    print "Ln(x) = " + str(approx_fx)
-    print "|Ln(x)-f(x)| = " + str(abs(approx_fx - Functions.f1(x_new)))
-    approx_f = get_approx_f_aitken(n,x,f)
-    draw_plot_aitken(x,f,approx_f)
+    approx_fx = Aitken.Ln(n, x, f, x_new)
+    print("Ln(x) = " + str(approx_fx))
+    print("|Ln(x)-f(x)| = " + str(abs(approx_fx - Functions.f1(x_new))))
+    text.insert(INSERT,
+                "Ln(x) = {}\n|Ln(x)-f(x)| = {}\n".format(str(approx_fx), str(abs(approx_fx - Functions.f1(x_new)))))
+
+    approx_f = get_approx_f_aitken(n, x, f)
+    draw_plot_aitken(x, f, approx_f)
 
 
 def main_trigonometry():
     x = get_Xs()
     f = get_Fs2(x)
     n = len(x) - 1
-    x_new = pi/13+1
+    x_new = pi / 13 + 1
     approx_fx = Trigonometry.solve(n, x, f, x_new)
-    print "Tn(x) = " + str(approx_fx)
-    print "|Tn(x)-f(x)| = " + str(abs(approx_fx - Functions.f2(x_new)))
-    approx_f = get_approx_f_trigonometry(n,x,f)
-    draw_plot_trigonometry(x,f,approx_f)
+    print("Tn(x) = " + str(approx_fx))
+    print("|Tn(x)-f(x)| = " + str(abs(approx_fx - Functions.f2(x_new))))
+    text.insert(INSERT,
+                "Tn(x) = {}\n|Tn(x)-f(x)| = {}\n".format(str(approx_fx), str(abs(approx_fx - Functions.f2(x_new)))))
+    approx_f = get_approx_f_trigonometry(n, x, f)
+    draw_plot_trigonometry(x, f, approx_f)
 
 
-main_aitken()
-main_trigonometry()
+def initUi(parent):
+    global text
+
+    frame1 = Frame(parent)
+    frame1.pack(fill=X)
+
+    buttonAitken = Button(frame1, text="Aitken", width=10, command=main_aitken)
+    buttonAitken.pack(padx=5, pady=5)
+
+    buttonTrigonometry = Button(frame1, text="Trigonometry", width=12, command=main_trigonometry)
+    buttonTrigonometry.pack(padx=5, pady=5)
+
+    frame2 = Frame(parent)
+    frame2.pack(fill=X)
+
+    text = Text(frame2, height=200, width=900)
+    text.pack(padx=5, pady=5)
+
+
+if __name__ == "__main__":
+    root.geometry("900x700+300+300")
+    initUi(root)
+    root.mainloop()
